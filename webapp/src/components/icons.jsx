8a251ee33d5fd080719f93d2fdd9479d910b2bac
Jesse Hallam <jesse.hallam@gmail.com>
2018-07-30 12:00:23 -0400

    tweak channel header icon
    This would better be handled by https://mattermost.atlassian.net/browse/MM-11386.

--- webapp/src/components/icons.jsx
+++ webapp/src/components/icons.jsx
@@ -5,5 +5,5 @@ export const MainMenuMobileIcon = () => (
 );
 
 export const ChannelHeaderButtonIcon = () => (
-    <i className='icon fa fa-plug'/>
+    <i className='icon fa fa-plug' style={{fontSize: '15px', position: 'relative', top: '-1px'}}/>
 );
