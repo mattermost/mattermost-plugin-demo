27771dbc51d0f634aff7fd7fe466412656f0d405
Hanzei <hanzei@mailbox.org>
2018-09-28 12:51:41 +0200

    Add demo usage of GetServerVersion

--- server/activate_hooks.go
+++ server/activate_hooks.go
@@ -3,41 +3,59 @@ package main
 import (
 	"fmt"
 
+	"github.com/blang/semver"
 	"github.com/mattermost/mattermost-server/model"
 )
+
+const minimumServerVersion = "5.4.0"
 
 // OnActivate is invoked when the plugin is activated.
 //
 // This demo implementation logs a message to the demo channel whenever the plugin is activated.
 func (p *Plugin) OnActivate() error {
-	teams, err := p.API.GetTeams()
+	v, err := semver.Parse(p.API.GetServerVersion())
 	if err != nil {
 		p.API.LogError(
-			"failed to query teams OnActivate",
+			"failed to parse server version",
 			"error", err.Error(),
 		)
+		return err
+	}
+	r := semver.MustParseRange(">=" + minimumServerVersion)
+	if !r(v) {
+		return fmt.Errorf("current Mattermost version is to low. Please update your Mattermost Server to a least v%s.", minimumServerVersion)
+	}
+
+	teams, appErr := p.API.GetTeams()
+	if appErr != nil {
+		p.API.LogError(
+			"failed to query teams OnActivate",
+			"error", appErr.Error(),
+		)
+		return appErr
 	}
 
 	for _, team := range teams {
-		if _, err := p.API.CreatePost(&model.Post{
+		_, appErr := p.API.CreatePost(&model.Post{
 			UserId:    p.demoUserId,
 			ChannelId: p.demoChannelIds[team.Id],
 			Message: fmt.Sprintf(
 				"OnActivate: %s", manifest.Id,
 			),
 			Type: "custom_demo_plugin",
-			Props: map[string]interface{}{
+			Props: model.StringInterface{
 				"username":     p.Username,
 				"channel_name": p.ChannelName,
 			},
-		}); err != nil {
+		})
+		if appErr != nil {
 			p.API.LogError(
 				"failed to post OnActivate message",
-				"error", err.Error(),
+				"error", appErr.Error(),
 			)
 		}
-
-		if err := p.registerCommand(team.Id); err != nil {
+		err := p.registerCommand(team.Id)
+		if err != nil {
 			p.API.LogError(
 				"failed to register command",
 				"error", err.Error(),
