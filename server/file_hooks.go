abc395ef1406f12182859abc40fef6e34489c9ea
Shobhit Gupta <smartyshobhit@gmail.com>
2019-03-12 05:12:13 -0700

    Add hooks for UserHasBeenCreated, FileWillBeUploaded (#23)
    * Add hooks for UserHasBeenCreated, FileWillBeUploaded
    
    * Update comment
    
    * Address comments

---
+++ server/file_hooks.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/mattermost/mattermost-server/model"
+	"github.com/mattermost/mattermost-server/plugin"
+)
+
+// FileWillBeUploaded is invoked when a file is uploaded, but before it is committed to backing store
+//
+// This demo implementation logs a message to the demo channel in the team
+// when a new file is uploaded.
+func (p *Plugin) FileWillBeUploaded(c *plugin.Context, fileInfo *model.FileInfo, reader bytes.Reader, buf *bytes.Buffer) {
+	configuration := p.getConfiguration()
+
+	if configuration.disabled {
+		return
+	}
+
+	teams, err := p.API.GetTeams()
+	if err != nil {
+		p.API.LogError(
+			"failed to query teams FileWillBeUploaded",
+			"error", err.Error(),
+		)
+		return
+	}
+
+	if reader.Size() == 0 {
+		p.API.LogError(
+			"Uploaded file has zero size",
+			"error", err.Error(),
+		)
+		return
+	}
+
+	for _, team := range teams {
+		msg := fmt.Sprintf("FileName @%s has been created in", fileInfo.Name)
+		if err := p.postPluginMessage(team.Id, msg); err != nil {
+			p.API.LogError(
+				"failed to post FileWillBeUploaded message",
+				"channel_id", configuration.demoChannelIds[team.Id],
+				"error", err.Error(),
+			)
+		}
+	}
+}
---
+++ server/user_hooks.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mattermost/mattermost-server/model"
+	"github.com/mattermost/mattermost-server/plugin"
+)
+
+// UserHasBeenCreated is invoked when a new user is created.
+//
+// This demo implementation logs a message to the demo channel in the team whenever a new user
+// is created.
+func (p *Plugin) UserHasBeenCreated(c *plugin.Context, user *model.User) {
+	configuration := p.getConfiguration()
+
+	if configuration.disabled {
+		return
+	}
+
+	teams, err := p.API.GetTeams()
+	if err != nil {
+		p.API.LogError(
+			"failed to query teams UserHasBeenCreated",
+			"error", err.Error(),
+		)
+		return
+	}
+
+	for _, team := range teams {
+		msg := fmt.Sprintf("User_ID @%s has been created in", user.Id)
+		if err := p.postPluginMessage(team.Id, msg); err != nil {
+			p.API.LogError(
+				"failed to post UserHasBeenCreated message",
+				"channel_id", configuration.demoChannelIds[team.Id],
+				"error", err.Error(),
+			)
+		}
+	}
+}
