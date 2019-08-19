c0944adfd2469a8e8be48201bbae574242decedd
Matthew Dorner <mstar444@mail.com>
2019-08-19 07:03:28 -0500

    Demonstrate use of this.props.hide() in popover components.

--- webapp/src/components/user_attributes.jsx
+++ webapp/src/components/user_attributes.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class UserAttributes extends React.PureComponent {
+    static propTypes = {
+        hide: PropTypes.func.isRequired,
+    }
+
+    onClick = () => {
+        this.props.hide();
+    }
+
     render() {
         return (
-            <div>{'Demo Plugin: User Attributes'}</div>
+            <div>
+                <a onClick={this.onClick}>
+                    {'Demo Plugin: User Attributes'}
+                </a>
+            </div>
         );
     }
 }
