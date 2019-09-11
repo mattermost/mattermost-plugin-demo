e1377f8f59d9d9293b0942f04dc62502417b2603
Hanzei <hanzei@mailbox.org>
2019-09-11 14:03:31 -0400

    Extend /dialog command to demonstrate generic error message in interactive dialog

--- server/command_hooks.go
+++ server/command_hooks.go
@@ -30,6 +30,7 @@ const (
 		"- `/dialog no-elements` - Open an Interactive Dialog with no elements. Once submitted, user's action is posted back into a channel.\n" +
 		"- `/dialog relative-callback-url` - Open an Interactive Dialog with relative callback URL. Once submitted, user's action is posted back into a channel.\n" +
 		"- `/dialog introduction-text` - Open an Interactive Dialog with optional introduction text. Once submitted, user's action is posted back into a channel.\n" +
+		"- `/dialog error` - Open an Interactive Dialog which always returns an general error.\n" +
 		"- `/dialog help` - Show this help text"
 )
 
@@ -251,6 +252,12 @@ func (p *Plugin) executeCommandDialog(args *model.CommandArgs) *model.CommandRes
 			TriggerId: args.TriggerId,
 			URL:       fmt.Sprintf("%s/plugins/%s/dialog/1", *serverConfig.ServiceSettings.SiteURL, manifest.Id),
 			Dialog:    getDialogWithIntroductionText(dialogIntroductionText),
+		}
+	case "error":
+		dialogRequest = model.OpenDialogRequest{
+			TriggerId: args.TriggerId,
+			URL:       fmt.Sprintf("/plugins/%s/dialog/error", manifest.Id),
+			Dialog:    getDialogWithoutElements(""),
 		}
 	default:
 		return &model.CommandResponse{
