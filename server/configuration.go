e593fba9bb822258702aaad25c95d5c68c7c66cf
Hanzei <hanzei@mailbox.org>
2020-06-05 14:46:09 +0200

    Fix demo user password

--- server/configuration.go
+++ server/configuration.go
@@ -241,7 +241,7 @@ func (p *Plugin) ensureDemoUser(configuration *configuration) (string, error) {
 	if user == nil {
 		user, err = p.API.CreateUser(&model.User{
 			Username:  configuration.Username,
-			Password:  "password",
+			Password:  "Password_123",
 			Email:     fmt.Sprintf("%s@example.com", configuration.Username),
 			Nickname:  "Demo Day",
 			FirstName: "Demo",
