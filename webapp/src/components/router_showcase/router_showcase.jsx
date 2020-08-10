ea666b9bfba682db9a70a9f68123b588b54b6d01
fmsilva1996 <fmsilva1996@gmail.com>
2020-08-10 16:52:21 +0000

    refactor: subpath links derived from function to improve readability

--- webapp/src/components/router_showcase/router_showcase.jsx
+++ webapp/src/components/router_showcase/router_showcase.jsx
@@ -13,29 +13,37 @@ export default function RouterShowcase() {
     const currentTeam = useSelector(getCurrentTeam);
     const query = useQuery();
 
+    function pushTeamUrlPath(path) {
+        return window.WebappUtils.browserHistory.push(teamUrlPath(path));
+    }
+
+    function teamUrlPath(path) {
+        return `/${currentTeam.name}/${id}/teamtest/${path}`;
+    }
+
     return (
         <Switch>
-            <Route path={`/${currentTeam.name}/${id}/teamtest/subpath`}>
+            <Route path={`${teamUrlPath('subpath')}`}>
                 <h3>{'Hello, Subpath Component!'}</h3>
             </Route>
-            <Route path={`/${currentTeam.name}/${id}/teamtest/subpath-with-query`}>
+            <Route path={`${teamUrlPath('subpath-with-query')}`}>
                 <h3>{'Hello, Subpath with Query!'}</h3>
-                <p>{`The search-value in the query string is "${query.get('search-value')}"`}</p>
+                <p>{`The search-value in the query string is ${query.get('search-value') === 'on' ? 'enabled' : 'disabled'}.`}</p>
             </Route>
             <Route>
                 <h3>{'Custom SubRoutes:'}</h3>
                 <li>
-                    <a onClick={() => window.WebappUtils.browserHistory.push(`/${currentTeam.name}/${id}/teamtest/subpath`)}>{'Subpath'}</a>
+                    <a onClick={() => pushTeamUrlPath('subpath')}>{'Subpath'}</a>
                 </li>
                 <li>
-                    <a onClick={() => window.WebappUtils.browserHistory.push(`/${currentTeam.name}/${id}/teamtest/subpath-with-query?search-value=mattermost-plugin`)}>{'Subpath with Query'}</a>
+                    <a onClick={() => pushTeamUrlPath('subpath-with-query?search-value=on')}>{'Subpath with Query'}</a>
                 </li>
             </Route>
         </Switch>
     );
 }
 
-// A custom hook that builds on useLocation to parse the query string for you.
+// A custom hook that builds on useLocation to parse the query string for you
 function useQuery() {
     return new URLSearchParams(useLocation().search);
 }
