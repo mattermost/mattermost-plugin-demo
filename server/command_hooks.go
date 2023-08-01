8b3cebadafffbe62a7839b23d114a49c1ab86694
Ben Schumacher <ben.schumacher@mattermost.com>
2023-08-01 21:20:07 +0200

    Make use of github.com/mattermost/mattermost/server/public

--- server/command_hooks.go
+++ server/command_hooks.go
@@ -7,8 +7,8 @@ import (
 
 	"github.com/pkg/errors"
 
-	"github.com/mattermost/mattermost-server/v6/model"
-	"github.com/mattermost/mattermost-server/v6/plugin"
+	"github.com/mattermost/mattermost/server/public/model"
+	"github.com/mattermost/mattermost/server/public/plugin"
 )
 
 const (
