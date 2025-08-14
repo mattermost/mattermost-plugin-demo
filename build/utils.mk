a69dde35ae732febc044db3c78d903606da86781
Felipe Martin <me@fmartingr.com>
2025-08-14 16:36:55 +0200

    refactor: pluginctl v0.1.5-0.20250814142442-19c417d53e1d

---
+++ build/utils.mk
@@ -0,0 +1,42 @@
+# ====================================================================================
+# Utilities
+# ====================================================================================
+
+## Clean removes all build artifacts.
+.PHONY: clean
+clean:
+	rm -fr dist/
+ifneq ($(HAS_SERVER),)
+	rm -fr server/coverage.txt
+	rm -fr server/dist
+endif
+ifneq ($(HAS_WEBAPP),)
+	rm -fr webapp/junit.xml
+	rm -fr webapp/dist
+	rm -fr webapp/node_modules
+endif
+	rm -fr build/bin/
+
+## Extract strings for translation from the source code.
+.PHONY: i18n-extract
+i18n-extract:
+ifneq ($(HAS_WEBAPP),)
+ifeq ($(HAS_MM_UTILITIES),)
+	@echo "You must clone github.com/mattermost/mattermost-utilities repo in .. to use this command"
+else
+	cd $(MM_UTILITIES_DIR) && npm install && npm run babel && node mmjstool/build/index.js i18n extract-webapp --webapp-dir $(PWD)/webapp
+endif
+endif
+
+## Generate mocks for testing.
+.PHONY: mock
+mock:
+ifneq ($(HAS_SERVER),)
+	go install github.com/golang/mock/mockgen@v1.6.0
+	mockgen -destination=server/command/mocks/mock_commands.go -package=mocks github.com/mattermost/mattermost-plugin-demo/server/command Command
+endif
+
+## Show help documentation.
+.PHONY: help
+help:
+	@cat Makefile build/*.mk | grep -v '\.PHONY' |  grep -v '\help:' | grep -B1 -E '^[a-zA-Z0-9_.-]+:.*' | sed -e "s/:.*//g" | sed -e "s/^## //g" |  grep -v '\-\-' | sed '1!G;h;$$!d' | awk 'NR%2{printf "\033[36m%-30s\033[0m",$$0;next;}1' | sort
