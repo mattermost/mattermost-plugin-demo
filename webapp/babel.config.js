5a50983779a6377ee335a552cf9a82d8e3fbd63f
Harrison Healey <harrisonmhealey@gmail.com>
2025-11-11 12:04:09 -0500

    Fix bundling on Mac
    This is the same fix from https://github.com/mattermost/mattermost-plugin-playbooks/pull/1745.

--- webapp/babel.config.js
+++ webapp/babel.config.js
@@ -25,9 +25,7 @@ const config = {
         }],
     ],
     plugins: [
-        '@babel/plugin-proposal-class-properties',
         '@babel/plugin-syntax-dynamic-import',
-        '@babel/proposal-object-rest-spread',
         'babel-plugin-typescript-to-proptypes',
     ],
 };
