9c80775f3d7261b350125d43ad9f2e4c6edad6f1
Scott Bishel <scott.bishel@mattermost.com>
2026-03-06 08:40:33 -0700

    feat: add RHS pop out demo to right-hand sidebar (#203)
    Converts RHSView to a functional component and adds two demo buttons
    showing how to use WebappUtils.popouts.popoutRhsPlugin — one via direct
    click handler and one via useEffect. Buttons are disabled on older
    servers that don't support the popout API.

--- webapp/src/components/right_hand_sidebar/rhs_view.jsx
+++ webapp/src/components/right_hand_sidebar/rhs_view.jsx
@@ -1,49 +1,98 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 
 import {FormattedMessage} from 'react-intl';
 
-export default class RHSView extends React.PureComponent {
-    static propTypes = {
-        team: PropTypes.object.isRequired,
-    }
+import {id as pluginId} from '../../manifest';
 
-    render() {
-        return (
-            <div style={style.rhs}>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
+export default function RHSView({team, channel}) {
+    const [autoPopout, setAutoPopout] = useState(false);
+    const popoutSupported = Boolean(window.WebappUtils?.popouts?.popoutRhsPlugin);
+
+    // useEffect example: automatically pop out the RHS when autoPopout is toggled on
+    useEffect(() => {
+        if (autoPopout) {
+            if (popoutSupported) {
+                window.WebappUtils.popouts.popoutRhsPlugin('Demo Plugin', pluginId, team.name, channel.name);
+            }
+            setAutoPopout(false);
+        }
+    }, [autoPopout, popoutSupported, team.name, channel.name]);
+
+    const handlePopout = () => {
+        if (popoutSupported) {
+            window.WebappUtils.popouts.popoutRhsPlugin('Demo Plugin', pluginId, team.name, channel.name);
+        }
+    };
+
+    return (
+        <div style={style.rhs}>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <FormattedMessage
+                id='rhs.triggered'
+                defaultMessage='You have triggered the right-hand sidebar component of the demo plugin.'
+            />
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <FormattedMessage
+                id='demo.testintl'
+                defaultMessage='This is the default string'
+            />
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            {'Links for custom routes'}
+            <br/>
+            <a onClick={() => window.WebappUtils.browserHistory.push('/plug/com.mattermost.demo-plugin/roottest')}>
+                {'/plug/com.mattermost.demo-plugin/roottest'}
+            </a>
+            <br/>
+            <a onClick={() => window.WebappUtils.browserHistory.push(`/${team.name}/com.mattermost.demo-plugin/teamtest`)}>
+                {`/${team.name}/com.mattermost.demo-plugin/teamtest`}
+            </a>
+            <br/>
+            <br/>
+            <hr/>
+            <br/>
+            <strong>{'Pop Out RHS Demo'}</strong>
+            <br/>
+            <br/>
+            <button
+                className='btn btn-primary'
+                disabled={!popoutSupported}
+                onClick={handlePopout}
+            >
                 <FormattedMessage
-                    id='rhs.triggered'
-                    defaultMessage='You have triggered the right-hand sidebar component of the demo plugin.'
+                    id='rhs.popout'
+                    defaultMessage='Pop Out RHS'
                 />
-                <br/>
-                <br/>
-                <br/>
-                <br/>
+            </button>
+            <br/>
+            <br/>
+            <button
+                className='btn btn-tertiary'
+                disabled={!popoutSupported}
+                onClick={() => setAutoPopout(true)}
+            >
                 <FormattedMessage
-                    id='demo.testintl'
-                    defaultMessage='This is the default string'
+                    id='rhs.popout.useeffect'
+                    defaultMessage='Pop Out via useEffect'
                 />
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                {'Links for custom routes'}
-                <br/>
-                <a onClick={() => window.WebappUtils.browserHistory.push('/plug/com.mattermost.demo-plugin/roottest')}>
-                    {'/plug/com.mattermost.demo-plugin/roottest'}
-                </a>
-                <br/>
-                <a onClick={() => window.WebappUtils.browserHistory.push(`/${this.props.team.name}/com.mattermost.demo-plugin/teamtest`)}>
-                    {`/${this.props.team.name}/com.mattermost.demo-plugin/teamtest`}
-                </a>
-            </div>
-        );
-    }
+            </button>
+        </div>
+    );
 }
+
+RHSView.propTypes = {
+    team: PropTypes.object.isRequired,
+    channel: PropTypes.object.isRequired,
+};
 
 const style = {
     rhs: {
